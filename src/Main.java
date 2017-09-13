6b0103a41fddb388eee4d596c6cd1266d2f15f3e
chsaipratyusha <sai.pratyushach16@gmail.com>
2017-09-13 12:17:11 +0530

    Add files via upload

---
+++ src/Main.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		
+		//You may test that your code works find here
+		//Please check that your code works and has no 
+		//compilation problems before to submit
+	Scanner sc=new Scanner(System.in);
+	int id,ch;
+	String fullName;
+	Date birthDate;
+	double avgMark;
+	do
+	{
+	 	id=sc.nextInt();
+		fullName=sc.next();
+		birthDate=sc.nextLine();
+	
+	}while(ch==1);
+
+
+		
+	}
+
+}
